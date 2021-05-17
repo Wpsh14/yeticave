520219cbe59a3c52156f89683d28d8b4ca97adae
Wpsh14 <79827681+Wpsh14@users.noreply.github.com>
2021-05-17 13:09:22 +0300

    Add files via upload

---
+++ templates/lot.php
@@ -0,0 +1,37 @@
+
+        <nav class="nav">
+            <ul class="nav__list container">
+                <?php foreach($categories_list as $item):?>
+                <li class="nav__item">
+                    <a href="all-lots.html"><?=$item['categ_name'];?></a>
+                </li>
+                <?php endforeach;?>
+            </ul>
+        </nav>
+        <section class="lot-item container">
+            <h2><?=$lot['lot_name'];?></h2>
+            <div class="lot-item__content">
+                <div class="lot-item__left">
+                    <div class="lot-item__image">
+                        <img src="<?=$lot['lot_image'];?>" width="730" height="548" alt="Сноуборд">
+                    </div>
+                    <p class="lot-item__category">Категория: <span><?=$categories_list[$lotID]['categ_name'];?></span></p>
+                    <p class="lot-item__description"><?=$lot['lot_descr_text']?></p>
+                </div>
+
+                <div class="lot-item__right">
+                    <div class="lot-item__state">
+                        <div class="lot-item__timer timer">
+                            <?=time_left($times_left);?>
+                        </div>
+                        <div class="lot-item__cost-state">
+                            <div class="lot-item__rate">
+                                <span class="lot-item__amount">Текущая цена</span>
+                                <span class="lot-item__cost"><?=format_sum($lot['lot_start_price']);?></span>
+                            </div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
